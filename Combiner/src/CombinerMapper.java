5d045fbf16ba97d13ed1b65c4a50200b438caf71
Harshit Dawar <31154755+HarshitDawar55@users.noreply.github.com>
2019-11-25 22:28:58 +0530

    Update and rename Combiner(Lab_7_A_part)/src/CombinerMapper.java to Combiner/src/CombinerMapper.java

--- Combiner/src/CombinerMapper.java
+++ Combiner/src/CombinerMapper.java
@@ -2,7 +2,6 @@
 import java.io.IOException;
 
 import org.apache.hadoop.io.DoubleWritable;
-//import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
