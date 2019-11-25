a4591dad8efc216eb5f1c523fc7818e243f8bcbd
Harshit Dawar <31154755+HarshitDawar55@users.noreply.github.com>
2019-11-25 22:36:03 +0530

    Update and rename Combiner(Lab_7_A_part)/src/CombinerClient.java to Combiner/src/CombinerClient.java

--- Combiner/src/CombinerClient.java
+++ Combiner/src/CombinerClient.java
@@ -1,7 +1,6 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
-//import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
