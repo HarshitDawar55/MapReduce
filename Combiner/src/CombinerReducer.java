3a9f1115f4e6ce25b09eac1f94a0cf1aafad63be
Harshit Dawar <31154755+HarshitDawar55@users.noreply.github.com>
2019-11-25 22:50:00 +0530

    Reducer for Combiner

--- Combiner/src/CombinerReducer.java
+++ Combiner/src/CombinerReducer.java
@@ -1,10 +1,6 @@
 import java.io.IOException;
-
 import org.apache.hadoop.io.DoubleWritable;
-//import org.apache.hadoop.io.IntWritable;
-
 import org.apache.hadoop.io.Text;
-
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class CombinerReducer extends
